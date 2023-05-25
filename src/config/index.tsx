a7ef9bcb501b0979fa4118eda8302ccc678ab18f
Matt Borgato <borgato.matteo@gmail.com>
2023-05-25 12:43:00 -0300

    ✨ revert MOCK_DATA_MODE to false

--- src/config/index.tsx
+++ src/config/index.tsx
@@ -3,4 +3,4 @@ export const DAYS_SPAN = 7
 export const GITHUB_URL = 'https://api.github.com/search/repositories'
 
 // use a static json instead of calling GH api
-export const MOCK_DATA_MODE = true
+export const MOCK_DATA_MODE = false
